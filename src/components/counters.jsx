7db01c7d6f2bb3994dde7f376ee9683432a0e5f5
Ujjwal Singhal <ujjwals@gmail.com>
2020-06-12 14:50:26 +0530

    add all files

---
+++ src/components/counters.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          onClick={this.props.onReset}
+          className="btn btn-success btn-lg m-2"
+        >
+          Reset
+        </button>
+        <button onClick={this.props.onAdd} className="btn btn-info btn-lg m-2">
+          Add Item
+        </button>
+        {this.props.counters.map((counter) => (
+          <Counter
+            key={counter.id}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <span className="badge badge-secondary badge-pill">
+          {this.props.numOfItems}
+        </span>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
