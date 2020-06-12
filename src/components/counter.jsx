3a507b5c8fba48d89254aebf26dd0a35fcf74afa
Ujjwal Singhal <ujjwals@gmail.com>
2020-06-12 15:10:24 +0530

    Changed button class from secondary to info

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,7 +12,7 @@ class Counter extends Component {
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
+          className="btn btn-info btn-sm"
         >
           Increment
         </button>
